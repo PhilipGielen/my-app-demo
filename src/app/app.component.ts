bd5a75aa1fe19d8a95f8a44ae32057c2aa254e29
PhilipGielen <philip.gielen@gmx.de>
2019-12-12 15:39:26 +0100

    "File Update"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app-demo';
+
+  navbarOpen = false;
+
+  toggleNavbar() {
+  this.navbarOpen = !this.navbarOpen;
+}
 }
